e585d3da950e2c529c4321346231bea39fb83dfc
andy920262 <920262@gmail.com>
2018-06-27 22:43:12 +0800

    Fix properties & add db

--- src/common/Properties.java
+++ src/common/Properties.java
@@ -6,17 +6,21 @@ import java.io.IOException;
 public class Properties {
 	public static java.util.Properties properties = new java.util.Properties();
     public static String configFile = "config.properties";
+    private static int port;
+    private static String host;
     static {
 	    try {
 	        properties.load(new FileInputStream(configFile));
+	        port = Integer.valueOf(properties.getProperty("PORT"));
+	        host = properties.getProperty("HOST");
 	    } catch (IOException ex) {
 	        ex.printStackTrace();
 	    }
     }
     public static int getPort() {
-    	return Integer.valueOf(properties.getProperty("PORT"));
+    	return port;
     }
     public static String getHost() {
-    	return properties.getProperty("Host");
+    	return host;
     }
 }
